7b31a893257b3f4abf6028c72854c67d348d6ec4
UrbinaArmando <urbinaa01@gmail.com>
2022-10-06 20:40:04 -0600

    se suben cambios

--- src/card.js
+++ src/card.js
@@ -2,7 +2,8 @@ const COLORS = require('./colors')
 
 const Card = (props) => {
     const view = `
-       <h1 style="background: ${COLORS.colors.Dark}">CARD3</h1>`
+       <h1 style="background: ${COLORS.colors.Dark}">CARD3</h1>
+    `
     return view;
 }
 
