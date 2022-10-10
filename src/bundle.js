f2c769d57f21263fde67bfbfbdedc29d1484a7db
Cesar.castrop <cesar.castrop@coppel.com>
2022-10-10 16:56:00 -0600

    Cambios esteticos

--- src/bundle.js
+++ src/bundle.js
@@ -1,48 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const COLOR = require('./colors');
-const TYPE = require('./pokemon-types');
-const POKEMONCLASS = require('./pokemon-class');
-
-// Pokemons from first region
-
-/*switch (pokemonRegion) {
-    case Kanto:
-        firstPokemon = 1;
-        lastPokemon = 151;
-        break;
-    case Johto:
-        firstPokemon = 152;
-        lastPokemon = 251;
-        break;
-    case Hoenn:
-        firstPokemon = 252;
-        lastPokemon = 386;
-        break;
-    case Sinnoh:
-        firstPokemon = 387;
-        lastPokemon = 493;
-        break;
-    case Unova:
-        firstPokemon = 494;
-        lastPokemon = 649;
-        break;
-    case Kalos:
-        firstPokemon = 650;
-        lastPokemon = 721;
-        break;
-    case Alola:
-        firstPokemon = 722;
-        lastPokemon = 809;
-        break;
-    case Galar:
-        firstPokemon = 810;
-        lastPokemon = 898;
-        break;
-    default:
-        break;
-}*/
-
-
+// Get pokemons from regions with switch cases from nav bar elements.
 const fetchPokemons = async (pokemonRegion) => {
     let firstPokemon;
     let lastPokemon;
@@ -89,55 +46,17 @@ const fetchPokemons = async (pokemonRegion) => {
     return pokemonsList;
 };
 
-// Get 151 pokemons from Pokeapi, fetchPokemons calls getPokemon 151 times.
+// Get pokemons from Pokeapi, fetchPokemons calls getPokemon.
 //And stores the data in 'const pokemon'.
 const getPokemon = async id => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const response = await fetch(url);
     const pokemon = await response.json();
-    //createPokemonCard(pokemon);
-    //console.log(pokemon)
     return pokemon;
 };
 
-/*// Function to create cards with pokemons data
-function createPokemonCard(pokemon) {
-
-    //CONST
-
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-    // Padstart for 3 numbers position with '0' at the start.
-    const id = pokemon.id.toString().padStart(3, '0')
-
-    // Get an array with pokemon types (Normal, Fire, water, etc.). Can be up to 2 types. First letter toUpperCase.
-    const pokemon_types = pokemon.types.map(type => type.type.name[0].toUpperCase() + type.type.name.slice(1));
-
-    const primaryType = pokemon_types[0];
-
-    const secondaryType = new Array(pokemon_types[1]).filter(item => item != undefined);
-
-    // Set the color using the name of the type and the colors array with types names.
-    const color = COLOR.colors[primaryType];
-
-    // Set the icons paths for the pokemon types
-    const icon1 = new Array(TYPE.TypeIcons[primaryType]);
-    const icon2 = new Array(TYPE.TypeIcons[secondaryType]).filter(item => item != undefined);
-
-    const height = pokemon.height / 10;
-
-    const weight = pokemon.weight / 10;
-
-    const ability = pokemon.abilities.map(ability => ability.ability.name[0].toUpperCase() + ability.ability.name.slice(1)).join(", ");
-
-    // Function to send dato to the card.
-    let POKEMONCLASS = new pokemonData(id, name, primaryType, secondaryType, color, icon1, icon2, height, weight, ability);
-
-}*/
-
-//fetchPokemons();
-
 module.exports.fetchPokemons = fetchPokemons;
-},{"./colors":3,"./pokemon-class":5,"./pokemon-types":6}],2:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 const COLORS = require("./colors");
 const POKEMONCLASS = require('./pokemon-class');
 
@@ -233,10 +152,11 @@ const navItemsList = [
     galarPage = document.getElementById("galar_page")
 ]
 //#endregion
+
 //#region  EVENTS
 navItemsList.forEach(async (e) => {
     e.addEventListener('click', async () => {
-        regionTitle.textContent = `${e.children[0].children[0].textContent}`;
+        regionTitle.textContent = `${e.children[0].children[0].textContent} Pokedex`;
         await fillScreen(e.id);
     })
 })
@@ -253,12 +173,6 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
-// Show active menu when scrolling
-const highlightMenu = () => {
-    const elem = document.querySelector('.highlight');
-
-    let scrollPos = window.scrollY;
-}
 //  Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
     const menuBars = document.querySelector('.is-active');
@@ -267,9 +181,19 @@ const hideMobileMenu = () => {
         menuLinks.classList.remove('active');
     }
 };
+//  Close normal Menu when clicking on a menu item
+const hideMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth >= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
 
 menuLinks.addEventListener('click', hideMobileMenu);
+menuLinks.addEventListener('click', hideMenu);
 //#endregion
+
 //#region FUNCTIONS
 const fillScreen = async (id) => {
     pokeball_loader.style.display = 'block';
